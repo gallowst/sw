778ed58f16253858b6d9840d549c1fd5a611fab1
Stuart Galloway <SGallowa@mpcuk.computacenter.com>
2021-12-16 16:41:16 +0000

    README

--- build.ps1
+++ build.ps1
@@ -1,43 +1,30 @@
-$strAcrName = "gallowsstarwarsacr"
 $strResourceGroupName = "gallows-euw-rg"
 $strLocation = "west europe"
 
-# Create the Resource Group and the ACR
+# Create the Resource Group
 New-AzResourceGroup -Location $strLocation -Name $strResourceGroupName
-$objAcr = New-AzContainerRegistry -ResourceGroupName $strResourceGroupName -Name $strAcrName -EnableAdminUser -Sku Basic
-# Build the Containers and get the registry credential
-az acr build --registry $strAcrName --image starwars-api .\api
-az acr build --registry $strAcrName --image starwars-app .\app
-az acr login --name $strAcrName
-# Build the credential
-$objCred = az acr credential show -n $strAcrName | ConvertFrom-Json
-$cred = New-Object System.Management.Automation.PSCredential ($objCred.username, $(ConvertTo-SecureString $objCred.passwords[0].value -AsPlainText -Force) )
-# Create the API Container
-$apiSplat = @{
-    ResourceGroupName  = $strResourceGroupName
-    Name               = "starwars-api"
-    Image              = ("{0}/starwars-api:latest" -f $objAcr.LoginServer)
-    Port               = @(5000)
-    RegistryCredential = $cred
-    RestartPolicy      = "Never"
-    IpAddressType      = "Public"
-    DnsNameLabel       = "starwars-api"
-}
 
-$objApi = New-AzContainerGroup @apiSplat
+# Create the API Container
+$apiPort1 = New-AzContainerInstancePortObject -Port 5000 -Protocol TCP
+$apiContainer = New-AzContainerInstanceObject -Name "starwars-api" -Image "docker.io/gallows/sw:api" -RequestCpu 0.5 -RequestMemoryInGb 0.5 -Port @($apiPort1)
+$apiContainerGroup = New-AzContainerGroup -ResourceGroupName $strResourceGroupName -Name "starwars-api" -Location $strLocation -Container $apiContainer -OsType Linux -RestartPolicy "Never" -IpAddressType Public -IPAddressDnsNameLabel starwars-api
+
 # Create the App Container
 $appSplat = @{
     ResourceGroupName  = $strResourceGroupName
     Name               = "starwars-app"
-    Image              = ("{0}/starwars-app:latest" -f $objAcr.LoginServer)
+    Image              = "docker.io/gallows/sw:app"
     Port               = @(80)
-    RegistryCredential = $cred
     RestartPolicy      = "Never"
     IpAddressType      = "Public"
     DnsNameLabel       = "starwars-app"
     EnvironmentVariable = @{"SW_URL"=$objApi.Fqdn}
 }
 
-$objApp = New-AzContainerGroup @appSplat
+$appenv1 = New-AzContainerInstanceEnvironmentVariableObject -Name "SW_URL" -Value $apiContainerGroup.IPAddressFqdn
+$appPort1 = New-AzContainerInstancePortObject -Port 80 -Protocol TCP
+$appContainer = New-AzContainerInstanceObject -Name "starwars-app" -Image "docker.io/gallows/sw:app" -RequestCpu 0.5 -RequestMemoryInGb 0.5 -Port @($appPort1) -EnvironmentVariable @($appEnv1)
+$appContainerGroup = New-AzContainerGroup -ResourceGroupName $strResourceGroupName -Name "starwars-app" -Location $strLocation -Container $appContainer -OsType Linux -RestartPolicy "Never" -IpAddressType Public -IPAddressDnsNameLabel starwars-app
+
 # View the returned content
-(Invoke-WebRequest ("http://{0}:{1}" -f $objApp.Fqdn, $objApp.Ports[0].PortProperty)).Content
+(Invoke-WebRequest ("http://{0}:{1}" -f $appContainerGroup.IpAddressFqdn, $appContainerGroup.IPAddressPort.Port1)).Content
