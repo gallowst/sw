2e11fec583777ed52898ef3784521c8d3b735fad
gallows <gallows@gallows.pw>
2023-01-28 21:59:13 +0000

    no trace

--- app/wsgi.py
+++ app/wsgi.py
@@ -3,32 +3,12 @@ import json
 import requests
 import socket
 import os
-from jaeger_client import Config
-from flask_opentracing import FlaskTracing
-from os import getenv
-import logging
-
-JAEGER_HOST = getenv('JAEGER_HOST', 'localhost')
 
 # Build the URL if the API server from an env var
 full_url = 'http://{url}:5000/api/v1/resources/random'.format(url=os.environ['SW_URL'])
 
 # Set up the application
 application = Flask(__name__,)
-
-log_level = logging.DEBUG
-logging.getLogger('').handlers = []
-logging.basicConfig(format='%(asctime)s %(message)s', level=log_level)
-# Create configuration object with enabled logging and sampling of all requests.
-config = Config(config={'sampler': {'type': 'const', 'param': 1},
-                        'logging': True,
-                        'local_agent':
-                        # Also, provide a hostname of Jaeger instance to send traces to.
-                        {'reporting_host': JAEGER_HOST}},
-               # Service name can be arbitrary string describing this particular web service.
-               service_name="starwars")
-jaeger_tracer = config.initialize_tracer()
-tracing = FlaskTracing(jaeger_tracer)
 
 # Define the web page
 @application.route("/")
